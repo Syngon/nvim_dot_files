aa676832a7f3bb5220c22187d5f5b7264a5bf93c
Pedro Monnerat <pedro.vianna@justtraveltour.com>
2023-08-04 23:59:37 -0300

    add remaps

--- lua/maps.lua
+++ lua/maps.lua
@@ -10,6 +10,21 @@ keymap.set('n', '<c-a>', 'ggVG')
 keymap.set('n', 'H', '^')
 keymap.set('n', 'L', '$')
 
-keymap.set('n', "j", [[v:count?'j':'gj']], {noremap = true, expr = true})
-keymap.set('n', "k", [[v:count?'k':'gk']], {noremap = true, expr = true})
+keymap.set('n', "j", [[v:count?'j':'gj']], { noremap = true, expr = true })
+keymap.set('n', "k", [[v:count?'k':'gk']], { noremap = true, expr = true })
 
+
+
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "<C-c>", "<Esc>:noh<cr>")
+vim.keymap.set("n", "<C-c>", "<Esc>:noh<cr>")
+
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
