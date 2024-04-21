4752b51b34071e38b9d2b6058be0bf295333dbf0
Pedro Monnerat <naotivecriatividadepraisso@gmail.com>
2024-04-21 19:25:30 -0300

    esc cancel search

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = ' '
 local keymap = vim.keymap
 
+
+keymap.set('n', "<Esc>", "<cmd>nohlsearch<CR>")
+
 keymap.set('n', '<leader>q', '<cmd>q<cr>')
 keymap.set('n', '<leader>w', '<cmd>w<cr>')
 keymap.set('n', '<leader>x', '<cmd>x<cr>')
